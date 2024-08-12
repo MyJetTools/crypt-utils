2454026844b2fea8e52f7a6c80bf64c0a530502c
amigin <amigin@gmail.com>
2024-08-12 18:21:40 +0300

    Fixes

--- src/rsa.rs
+++ src/rsa.rs
@@ -1,7 +1,6 @@
-use openssl::{rsa::Rsa, error::ErrorStack};
+use openssl::{error::ErrorStack, rsa::Rsa};
 
-
-pub fn generate_rsa_keypeir() -> Result<(Vec<u8>, Vec<u8>), ErrorStack>{
+pub fn generate_rsa_key_pair() -> Result<(Vec<u8>, Vec<u8>), ErrorStack> {
     let rsa = Rsa::generate(2048)?;
     let private_key: Vec<u8> = rsa.private_key_to_pem()?;
     let public_key: Vec<u8> = rsa.public_key_to_pem()?;
@@ -9,7 +8,7 @@ pub fn generate_rsa_keypeir() -> Result<(Vec<u8>, Vec<u8>), ErrorStack>{
     Ok((private_key, public_key))
 }
 
-pub fn get_public_rsa_key_from_private(private_key: Vec<u8>) -> Result<Vec<u8>, ErrorStack>{
+pub fn get_public_rsa_key_from_private(private_key: Vec<u8>) -> Result<Vec<u8>, ErrorStack> {
     let private = Rsa::private_key_from_pem(&private_key.clone()).unwrap();
     Ok(private.public_key_to_pem()?)
 }
